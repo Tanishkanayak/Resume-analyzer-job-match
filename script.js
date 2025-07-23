cb2f332aed0abcf69b6096f3dbee363114b39353
Tanishkanayak <139033737+Tanishkanayak@users.noreply.github.com>
2025-07-23 20:28:47 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const form = document.querySelector("form");
-  form.addEventListener("submit", () => {
-    const button = form.querySelector("button");
-    button.innerText = "Analyzing...";
-    button.disabled = true;
-  });
-});
